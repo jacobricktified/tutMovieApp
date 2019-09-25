abf811010373b045fe03dac5d602df8bc7a432ed
jacob mbuco <mbucojacob@gmail.com>
2019-09-25 12:25:51 +0300

    work on movie java class

--- app/src/main/java/com/example/movieapp/MainActivity.java
+++ app/src/main/java/com/example/movieapp/MainActivity.java
@@ -2,13 +2,93 @@ package com.example.movieapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.GridLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.movieapp.model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private MoviesAdapter adapter;
+    private List<Movie> movieList;
+    ProgressDialog pd;
+    private SwipeRefreshLayout swipeContainer;
+    public static final String LOG_TAG= MoviesAdapter.class.getName();
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+      public Activity gotActivity() {
+          Context context = this;
+          while (context instanceof ContextWrapper){
+              if (context instanceof  Activity){
+                  return (Activity) context;
+
+              }
+              context = ((ContextWrapper) context).getBaseContext();
+          }
+
+          return null;
+
+
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+        swipeContainer=(SwipeRefreshLayout) findViewById(R.id.main_content)
+       swipeContainer.setColorSchemeResources(android.R.color.holo_orange_dark);
+        swipeContainer.setOnRefreshListener(new SwipefreshLayout.OnRefreshListener(){
+
+            @Override
+            public void onRefresh (){
+                Toast.makeText(MainActivity.this."Movies Refreshed", Toast.LENGTH_SHORT).show();
+            }
+
+
+
+        });
+        }
     }
-}
+    private  void initView(){
+
+    pd = new ProgressDialog(this);
+    pd.setMessage("Fetching movies....");
+    pd.setCancelable(false);
+    pd.show();
+
+    recyclerView= (Recyclerview) findViewById(R.id.recycler_view);
+    movieList =new ArrayList<>();
+    adapter = new MoviesAdapter(this.movieList);
+    if(getActivity().getResources ().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+        recyclerView.setLayoutManger(new GridLayout(this,2));
+
+    }else{
+        recyclerView.setLayout(new GridLayout(this, 4));
+    }
+    recyclerView.setItemAnimator(new DefaultItemAnimator());
+    recyclerView.setAdapter(adapter);
+    adapter.notifyDataSetChanged();
+    loadJSON();
+}}
+    private void loadJSON {
+
+
+        }
+
+
--- app/src/main/java/com/example/movieapp/model/Movie.java
+++ app/src/main/java/com/example/movieapp/model/Movie.java
@@ -1,4 +1,43 @@
 package com.example.movieapp.model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 public class Movie {
+    @SerializedName("poster path")
+    private String posterPath;
+    @SerializedName("adult")
+    private boolean adult;
+    @SerializedName("overview")
+    private boolean overview;
+    @SerializedName("release_date")
+    private boolean releaseDate;
+    @SerializedName("gener_ids")
+    private List<Integer> genreIds = new ArrayList<Integer>();
+    @SerializedName("id")
+    private boolean id;
+    @SerializedName("original_title")
+    private boolean originalTitle;
+    @SerializedName("original_language")
+    private boolean originalLanguage;
+    @SerializedName("title")
+    private boolean title;
+    @SerializedName("backdrop_path")
+    private boolean backdropPath;
+    @SerializedName("popularity")
+    private boolean popularity;
+    @SerializedName("vote_count")
+    private boolean votecount;
+    @SerializedName("video")
+    private boolean video;
+    @SerializedName("vote_average")
+    private boolean voteAverage;
+
+
+
+
+
+
 }
